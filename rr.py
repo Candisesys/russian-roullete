08311bae81715c3427d841d75647fdabf80c349a
Candisesys <johnstove482@gmail.com>
2021-05-17 13:03:23 -0400

    Update rr.py

--- rr.py
+++ rr.py
@@ -1,30 +1,36 @@
 import random
 import time
 import os
-os.system("cls")
+def clear():
+    try:
+        os.system("cls")
+    except:
+        os.system("clear")
 # defining variables and functions
-x = 1
-shoot = input("Are you going to shoot? Type 'y' for Yes, Type anything else for no ")
-# code
-x = random.randint(1,6)
-if shoot == "y":
-    print(x)
-    if x == 6:
-        os.system("cls")
-        print(">O<-<")
-        time.sleep(1)
-        os.system("cls")
-        print("> O/-<")
-        time.sleep(1)
-        exit()
-    else:
-        os.system("cls")
-        print(">O<-<")
-        time.sleep(1)
-        os.system("cls")
-        print("  v YAY!!! I DIDNT DIE!!!")
-        print("> O<-<")
-        time.sleep(1)
-        exit()
-else:
-    exit()
+while True:
+    x = 1
+    shoot = input("Are you going to shoot? Type 'y' for Yes, Type anything else for no ")
+    # code
+    clear()
+    x = random.randint(1,6)
+    if shoot == "y":
+        print(x)
+        if x == 6:
+            os.system("cls")
+            print(">O<-<")
+            time.sleep(1)
+            os.system("cls")
+            print("> O/-<")
+            time.sleep(1)
+            exit()   
+        else:
+            os.system("cls")
+            print(">O<-<")
+            time.sleep(1)
+            os.system("cls")
+            print("  v YAY!!! I DIDNT DIE!!!")
+            print("> O<-<")
+            time.sleep(1)
+            #exit()
+    #else:
+     #   exit()
